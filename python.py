5bab8c2f92903d7fee8f354d344fb6780bc183d1
Akshath <kpakshath@gmail.com>
2023-08-12 15:08:12 +0530

    Updated python.py and added the loop necessary for exiting the application on demand
    Added the exit feature!

--- python.py
+++ python.py
@@ -1,28 +1,38 @@
 import time
 
-gamePrompt = ""
-userName = input("What is your username:  ")
-print("Hello, " + userName +". Welcome to Game Reviewer!\n")
-gamePrompt = input("Enter the name of the game:  ")
+def gamePrompt():
+    userName = input("What is your username:  ")
+    print("Hello, " + userName +". Welcome to Game Reviewer!\n")
 
-if gamePrompt == "GTA 5":
-       print("GTA 5 gets 94/100")
-       time.sleep(5.5)
-elif gamePrompt == "Fifa 20":
-       print("Fifa 20 gets 87/100")
-       time.sleep(5.5)
-elif gamePrompt == "Fifa 21":
-       print("Fifa 21 gets 89/100")
-       time.sleep(5.5)
-elif gamePrompt == "Fifa 22":
-       print("Fifa 22 gets 93/100")
-       time.sleep(5.5)
-elif gamePrompt == "Fifa 23":
-       print("Fifa 23 gets 96/100")
-       time.sleep(5.5)
-elif gamePrompt == "Cyberpunk 2077":
-       print("Cyberpunk gets 63/100")
-       time.sleep(4.5)
+    while True:
+        game_name = input("Enter the name of the game (or 'exit' to quit):  ")
+        
+        if game_name.lower() == "exit":
+            print("Exiting...")
+            break
 
+        if game_name == "GTA 5":
+            print("GTA 5 gets 94/100")
+        elif game_name == "Fifa 20":
+            print("Fifa 20 gets 87/100")
+        elif game_name == "Fifa 21":
+            print("Fifa 21 gets 89/100")
+        elif game_name == "Fifa 22":
+            print("Fifa 22 gets 93/100")
+        elif game_name == "Fifa 23":
+            print("Fifa 23 gets 96/100")
+        elif game_name == "Cyberpunk 2077":
+            print("Cyberpunk gets 63/100")
+        elif game_name == "Fifa Mobile":
+            print("Fifa Mobile gets 91/100")
+        else:
+            print("Game not found.")
+
+        choice = input("Do you want to exit (y/n)? ").lower()
+        if choice == "y":
+            print("Exiting...")
+            break
+
+gamePrompt()
 
 
